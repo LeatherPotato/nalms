2e2b57dfd86ee0160e3fa4e9366e299d6a651706
TheSussyWussy <81490857+TheSussyWussy@users.noreply.github.com>
2023-11-24 10:05:36 +0000

    first commit 🚀 initialised repo, wrote some basic delivery code and boilerplate, etc.

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='client/build', static_url_path='/')
+
+# serve react app
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        # returns error page 
+        # FIXME edit later to return actual error page
+        return send_from_directory(app.static_folder, 'index.html')
+
+# API/server code
+# remnember to put all utoils 
+
+@app.route('/')
+def hello():
+    return 'ellow, mate'
+
+@app.route('/api/')
+def api_home():
+    return 'welcome to the NALMS api'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
